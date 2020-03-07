a867fe1486778e5fb774d5187ec2249d20bc2f79
Graham <GrahamPayne4@gmail.com>
2020-03-07 10:34:01 +0200

    Added dynamic array class

---
+++ DataStructures/DataTypes/DynamicArray.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.DataTypes
+{
+    public class DynamicArray
+    {
+        int current;
+        int size = 4;
+        int[] values;
+
+        public DynamicArray(int newValue)
+        {
+            values = new int[size];
+            current = 0;
+            values[current] = newValue;
+            current++;
+            Console.WriteLine("Creating Dynamic Array with 1st value {0}", newValue);
+        }
+
+        public void Add(int newValue)
+        {
+            Console.WriteLine("Adding value {0} at {1}", newValue, current);
+            if((current+1)<(size-1))
+            {
+                values[current] = newValue;
+                current++;
+            }
+            else
+            {
+                int[] newValues = new int[size * 2];
+                size = size * 2;
+                for(int i = 0; i < current; i++)
+                {
+                    newValues[i] = values[i];
+                }
+                newValues[current] = newValue;
+                values = (int[]) newValues.Clone();
+                current++;
+            }
+        }
+
+        public void delete(int newValue)
+        {
+            int indexValue = -1;
+            for(int i =0; i<current; i++)
+            {
+                if(newValue == values[i])
+                {
+                    indexValue = i;
+                }
+            }
+
+            if (indexValue > -1)
+            {
+                Console.WriteLine("Deleting value {0} at {1}", newValue, indexValue);
+                for(int i=indexValue; i < current-1; i++)
+                {
+                    values[i] = values[i + 1];
+                }
+                current--;
+            }
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < current; i++)
+            {
+                Console.Write(" {0} ", values[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,19 @@
+﻿using DataStructures.DataTypes;
+using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dynamic Array ");
+            var dynamicArray = new DynamicArray(5);
+            dynamicArray.Add(2);
+            dynamicArray.Add(4);
+            dynamicArray.print();
+            dynamicArray.delete(4);
+            dynamicArray.print();
+        }
+    }
+}
